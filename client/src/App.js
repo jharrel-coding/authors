63435ebf27b1b93110e23e78a57c6be48e1a88c6
Jason Harrel <jharreldesign@gmail.com>
2022-08-15 12:58:33 -0700

    Favorite Authors

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayAll from './components/DisplayAll';
+import AuthorForm from './components/AuthorForm';
+import EditAuthor from './components/EditAuthor';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DisplayAll/>} path="/" />
+            <Route element={<AuthorForm/>} path="/new" />
+            <Route element={<EditAuthor/>} path="/edit/:id" />
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
